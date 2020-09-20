ff30661f66f4e73cf8ee16da58bc687a8a3c3a6e
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2020-09-20 19:27:16 +0200

    little fix to errorHandler, updated tests

--- src/utils/errorHandlers/ErrorBox.jsx
+++ src/utils/errorHandlers/ErrorBox.jsx
@@ -3,7 +3,7 @@ import { ErrorContainter } from "../../styles/ContainerStyles";
 import { ErrorBtn } from "../../styles/ButtonStyles";
 
 export const ErrorBox = () => (
-  <ErrorContainter>
+  <ErrorContainter role="alert">
     <p>Ops seems like there is some problem. Please reload page</p>
     <ErrorBtn onClick={() => window.location.reload()}>Reload page</ErrorBtn>
   </ErrorContainter>
